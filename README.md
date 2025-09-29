4ee3c4696859b37573b4500813cd2ea63af84e61
Rickard Nilsson <rickynils@gmail.com>
2025-09-29 11:05:47 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # nixbuild-checks
+
+Experimental action for creating Check Runs to run asynchrounous builds on
+nixbuild.net. This action may be integrated into
+[nixbuild-action](https://github.com/nixbuild/nixbuild-action) in the future, or
+not.
+
+## Overview
+
+## Usage
---
+++ evaluate.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -e
+
+# Parse Nix args
+nix_args=()
+if [ -n "$1" ]; then
+  IFS='\n'
+  while read l; do
+    readarray -d ' ' -O "${#nix_args[@]}" -t nix_args < <(echo -n "$l")
+  done < <(echo "$1")
+fi
+shift
+
+# Collect installables from the JSON objects passed as args
+nix_installables=()
+for x; do
+  nix_installables+=(
+    "$(jq -nr --argjson x "$x" '"\(env.FLAKE_URL)#\(env.FLAKE_ATTR).\($x.attr)"')"
+  )
+done
+
+# Use a dedicated eval cache for the given set of installables to work around
+# non-thread-safe Nix eval cache.
+cache_dir="$XDG_CACHE_HOME/$(echo "${nix_installables[@]}" | md5sum | cut -b-8)"
+
+# Evaluate the installables in one go. This enables eval sharing within Nix.
+XDG_CACHE_HOME="$cache_dir" nix path-info \
+  --derivation "${nix_args[@]}" "${nix_installables[@]}" >/dev/null
+
+# Loop through the (now evaluated) installables one at a time. This is so we
+# can match the installable with its resulting drv-file
+for x; do
+  nix_installable+=(
+    "$(jq -nr --argjson x "$x" '"\(env.FLAKE_URL)#\(env.FLAKE_ATTR).\($x.attr)"')"
+  )
+  name="$(jq -nr --argjson x "$x" '$x.label')"
+  title="Build $(jq -nr --argjson x "$x" '"\(env.FLAKE_ATTR).\($x.attr)"')"
+
+  # Retrieve the drv path from the attribute
+  drv="$(XDG_CACHE_HOME="$cache_dir" nix path-info \
+    --derivation "${nix_args[@]}" "$nix_installable"
+  )"
+
+  # Print a JSON line with info about the installable
+  # Grab output lock to avoid intermingled lines when this runs with xargs
+  flock 9 jq -cn \
+    --arg name "$name" \
+    --arg title "$title" \
+    --arg drv "$drv" \
+    --arg cache_dir "$cache_dir" '
+      { name: $name
+      , title: $title
+      , drv: $drv
+      , cache_dir: $cache_dir
+      }
+    '
+done
